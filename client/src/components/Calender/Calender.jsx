42f31a417ef51761363f31c86537d98c1101466d
Muhammedsuwaneh <suwanehmuhammed1@gmail.com>
2023-05-02 16:56:09 +0300

    calender feature

--- client/src/components/Calender/Calender.jsx
+++ client/src/components/Calender/Calender.jsx
@@ -3,12 +3,16 @@ import { Box, Stack, Typography, Grid } from "@mui/material";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PageTitle from '../UI/PageTitle';
 
+import MaterialCalendar from './MaterialCalender';
+
 export default function Calender() {
   return (
     <Stack sx={{ padding: { lg: "2rem", sm: "1rem", xs: "1rem", md: "1rem"}}}>
           <PageTitle title="Calender">
             <CalendarMonthIcon sx={{ color: "#0F4AC7" }} />
-        </PageTitle>
+          </PageTitle>
+          <MaterialCalendar defaultDate={new Date()}/>
     </Stack>
   )
 }
+
---
+++ client/src/components/Calender/MaterialCalender.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Box, Grid, Typography, IconButton } from '@mui/material';
+import { ChevronLeft, ChevronRight } from '@mui/icons-material/';
+
+const MaterialCalendar = ({ defaultDate }) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handlePrevMonth = () => {
+    setSelectedDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
+  };
+
+  const handleNextMonth = () => {
+    setSelectedDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  };
+
+  const renderCalendarHeader = () => {
+    return (
+      <Box sx={{ display: "flex", justifyContent:"space-between", margin: "1rem auto", alignItems: "center", background: "#fff", padding: "1rem", borderRadius: "1rem" }}>
+        <Typography variant="h5" align="center">
+            {selectedDate.toLocaleString('default', { month: 'long', year: 'numeric' })}
+        </Typography>
+        <Box>
+            <IconButton onClick={handlePrevMonth}>
+                <ChevronLeft />
+            </IconButton>
+            <IconButton onClick={handleNextMonth}>
+                <ChevronRight />
+            </IconButton>
+        </Box>
+      </Box>
+    );
+  };
+
+
+  const renderCalendarBody = () => {
+    const daysInMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0).getDate();
+    const firstDayOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay();
+    const days = [];
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const currentDay = defaultDate.getDate(); 
+
+    const dayLabels = daysOfWeek.map(day => (
+        <Grid item key={day}>
+          <Typography variant="body1" align="center">
+            {day}
+          </Typography>
+        </Grid>
+      ));
+
+    for (let i = 1; i <= daysInMonth + firstDayOfMonth - 1; i++) {
+      if (i < firstDayOfMonth) {
+        days.push(<Grid item key={i}></Grid>);
+      } else {
+        const date = i - firstDayOfMonth + 1;
+        days.push(
+          <Grid item key={i}>
+             <Box sx={{ height: "100px", background: "#fff", padding: "1rem", borderRadius: "1rem", cursor: "pointer",
+                background: `${(currentDay == date && defaultDate.getMonth() == selectedDate.getMonth()) ? "#1976D2" : "#fff"}`}}>
+                <Typography variant="body1" align="center" color={`${(currentDay == date && defaultDate.getMonth() == selectedDate.getMonth()) ? "#fff" : "#000"}`}>
+                  {date}
+                </Typography>
+            </Box>
+          </Grid>
+        );
+      }
+    }
+
+    return (
+        <>
+            <Grid item container spacing={1} sx={{ display: "grid", gridTemplateColumns: "repeat(7,1fr)", marginBottom: "1rem"}}>
+                {dayLabels}
+            </Grid>
+            <Grid container spacing={1} sx={{ display: "grid", gridTemplateColumns: "repeat(7,1fr)"}}>
+                {days}
+            </Grid>
+        </>
+    );
+  };
+
+  return (
+    <Grid container direction="column" spacing={1} sx={{ width: "100%", padding: "0" }}>
+      <Grid item>{renderCalendarHeader()}</Grid>
+      <Grid item>{renderCalendarBody()}</Grid>
+    </Grid>
+  );
+};
+
+export default MaterialCalendar;
